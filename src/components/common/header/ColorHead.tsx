1a7c9f8ec701ba72a9c09769d7d50081f7eb2da3
Goldenprevue <goldenprevue@gmail.com>
2023-06-02 17:52:32 +0900

    ✨.EDIT 헤더 반응형&icon jsx문법 경고문 해결

--- src/components/common/header/ColorHead.tsx
+++ src/components/common/header/ColorHead.tsx
@@ -3,11 +3,11 @@ import { ILogo } from '../icon/Icon';
 
 export default function ColorHead() {
   return (
-    <div className="bg-deepBlue w-screen h-[60px] text-white flex justify-between px-[240px]">
+    <div className="bg-deepBlue w-screen h-[60px] items-center text-white flex justify-between gap-10  px-[140px]">
       <ILogo />
-      <div>
-        <div className="user">로그인</div>
-        <div className="user">회원가입</div>
+      <div className="flex-row shrink-0 hidden sm:flex gap-4">
+        <div className="user ">로그인 </div>/
+        <div className="sign shrink-0">회원가입</div>
       </div>
     </div>
   );
