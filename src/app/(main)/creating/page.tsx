69dffb3abf6d6a24f9fa87e7a7a322b1270173dd
Goldenprevue <goldenprevue@gmail.com>
2023-06-04 02:48:19 +0900

    ADD.create page

--- src/app/(main)/creating/page.tsx
+++ src/app/(main)/creating/page.tsx
@@ -1,10 +1,52 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { CreateUTM } from '@/components/create/CreateUTM';
+import styles from './createutm.module.css';
+import Image from 'next/image';
+import { CreateCopyBox } from '@/components/create/CreateCopyBox';
 
-export default function CreatePage() {
+export default function CreateUTMPage() {
+  const [resUTM, setResUTM] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+  }, []);
   return (
     <>
-    <h1>생성하기</h1>;
+      {isLoading && (
+        <div className={styles.create_container}>
+          <h1>새 UTM 생성하기</h1>
+          <div className={styles.help_box}>
+            <p className={styles.sub_title}>
+              UTM은 한번에 5개까지 생성할 수 있습니다.
+            </p>
+            {/* <div className={styles.hlep_text}>
+            ❓ UTM 생성을 어떻게 해야할 지 모르겠다면
+            <Link
+              href={
+                "https://unexpected-ceder-0b7.notion.site/0a3db0d8103f4be2855a23186fc1b5e3"
+              }
+              target='_blank'>
+              {" "}
+              가이드보기{" "}
+            </Link>
+          </div> */}
+
+            <p className={styles.help_text}>*는 필수 입력 값입니다.</p>
+          </div>
+
+          <CreateUTM setResUTM={setResUTM} resUTM={resUTM} />
+          <div className={styles.copy_help_box}>
+            <p>{''}</p>
+            <p className={styles.copy_help}>
+              Shorten URL을 사용하시면 유렉카를 통해 간단한 트래킹 결과를
+              확인하실 수 있습니다.
+            </p>
+          </div>
+          <CreateCopyBox resUTM={resUTM} />
+        </div>
+      )}
     </>
-  )
-  
+  );
 }
---
+++ src/components/create/CreateUTM.tsx
@@ -0,0 +1,380 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useFieldArray, useForm } from 'react-hook-form';
+
+import styles from './CreateUTM.module.css';
+// import plus from 'public/assets/img/plus.png';
+// import minus from 'public/assets/img/minus.png';
+import Image from 'next/image';
+// import { BlueButton } from "@/src/common/button/blue_button/BlueButton";
+import { CreateCategory } from './CreateCategory';
+// import Alert from "@/src/common/button/Alert";
+// import { getCookie } from "src/util/async/Cookie";
+import { redirect } from 'next/navigation';
+// import { useMutation } from "@tanstack/react-query";
+// import Loading from "@/src/common/modal/Loading";
+import { Tooltip } from '@mui/material';
+// import { createUTMs } from "@/src/service/create";
+export type UTMsType = {
+  utms: {
+    utm_url?: string;
+    utm_campaign_id?: string;
+    utm_source?: string;
+    utm_medium?: string;
+    utm_campaign_name?: string | null;
+    utm_content?: string | null;
+    utm_term?: string | null;
+    utm_memo?: string | null;
+  }[];
+};
+type PropsType = {
+  setResUTM: any;
+  resUTM: any;
+};
+export const CreateUTM: React.FC<PropsType> = ({ setResUTM }) => {
+  const [memoText, setMemoText] = useState('');
+  const [alert, setAlert] = useState(false);
+
+  //   const { mutate, isLoading, data } = useMutation(postUTMs);
+  // const { mutate, isLoading, data } = useMutation(createUTMs);
+  const source = [
+    'band',
+    'daum',
+    'facebook',
+    'google',
+    'instagram',
+    'kakaotalk',
+    'line',
+    'naver',
+    'newsletter',
+    'tiktok',
+    'youtube',
+  ];
+  const mediumSelect = [
+    ' affiliates',
+    ' banner',
+    ' blog',
+    ' cpc',
+    ' cpm',
+    ' display',
+    ' email',
+    ' notification',
+    ' paidsearch',
+    ' referral',
+    ' social',
+    ' socialmedia',
+    ' sms',
+  ];
+  // const res = data?.data;
+  // setResUTM(res);
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors },
+  } = useForm<UTMsType>({
+    defaultValues: {
+      utms: [
+        {
+          utm_url: '',
+          utm_campaign_id: '',
+          utm_source: '',
+          utm_medium: '',
+          utm_campaign_name: null,
+          utm_term: null,
+          utm_content: null,
+          utm_memo: null,
+        },
+      ],
+    },
+    mode: 'onBlur',
+  });
+  const { fields, append, remove } = useFieldArray({
+    name: 'utms',
+    control,
+  });
+  // const requeirFn = (e: any) => {
+  //   e.target.value = e.target.value.replace(/[^a-z0-9./:_-]?/, '');
+  //   e.target.value = e.target.value.replace({ maxLength: 70 }, '');
+  // };
+
+  const addList = () => {
+    if (fields.length <= 4) {
+      append({
+        utm_url: '',
+        utm_campaign_id: '',
+        utm_source: '',
+        utm_medium: '',
+        utm_campaign_name: '',
+        utm_content: '',
+        utm_term: '',
+        utm_memo: '',
+      });
+    }
+  };
+  const memoHandler = (e: any) => {
+    const textareaValue = e.target?.value;
+    setMemoText(textareaValue);
+  };
+
+  const onSubmit = async (data: UTMsType) => {
+    // try {
+    //   mutate(data);
+    // } catch (e) {
+    //   console.log(e);
+    // }
+  };
+  useEffect(() => {}, [memoText]);
+
+  /**
+   * 로그인 하지 않은 유저 로그인 페이지로 보내기
+   */
+  useEffect(() => {
+    // const cookie = getCookie("access_token");
+    // if (!cookie) {
+    //   redirect("/login");
+    // }
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.scroll_input}>
+          <CreateCategory />
+          <div className={styles.create_container}>
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id}>
+                  <div key={field.id}>
+                    <div className={styles.item_box}>
+                      <div className={styles.number}>{index + 1}</div>
+                      <Tooltip title={'(필수) 연결할 URL을 입력해 주세요.'}>
+                        <input
+                          placeholder="https://를 붙여서 입력해 주세요."
+                          type="url"
+                          {...register(`utms.${index}.utm_url` as const, {
+                            required: true,
+                            maxLength: 200,
+                          })}
+                          className={`${
+                            errors?.utms?.[index]?.utm_url
+                              ? styles.error
+                              : styles.input_style
+                          }`}
+                        />
+                      </Tooltip>
+                      <datalist id="source">
+                        {source.map((i, idx) => (
+                          <div key={idx}>
+                            <option value={i} />
+                          </div>
+                        ))}
+                      </datalist>
+                      <Tooltip
+                        title={
+                          '(필수) 유저가 어디에서 인입되는지 출처를 입력해 주세요.'
+                        }
+                      >
+                        <input
+                          list="source"
+                          placeholder="ex) google, naver, facebook…"
+                          {...register(`utms.${index}.utm_source` as const, {
+                            required: true,
+                            maxLength: 20,
+                          })}
+                          className={`${
+                            errors?.utms?.[index]?.utm_source
+                              ? styles.error
+                              : styles.input_style
+                          }`}
+                        />
+                      </Tooltip>
+                      <datalist id="medium">
+                        {mediumSelect.map((i, idx) => (
+                          <div key={idx}>
+                            <option>{i}</option>
+                          </div>
+                        ))}
+                      </datalist>
+                      <Tooltip
+                        title={
+                          '(필수) 유저가 어떤 방식을 통해 인입되는지 홍보 방법을 입력해 주세요.'
+                        }
+                      >
+                        <input
+                          list="medium"
+                          placeholder="ex) email, display, cpc…"
+                          {...register(`utms.${index}.utm_medium` as const, {
+                            required: true,
+                            maxLength: 20,
+                            // pattern: /[a-z]/i,
+                          })}
+                          className={`${
+                            errors?.utms?.[index]?.utm_medium
+                              ? styles.error
+                              : styles.input_style
+                          }`}
+                        />
+                      </Tooltip>
+                      <Tooltip
+                        title={
+                          '(필수) 어떤 이벤트/캠페인을 진행하고 있는지 홍보 명을 입력해 주세요.'
+                        }
+                      >
+                        <input
+                          placeholder="ex) close_beta, open_beta, open…"
+                          {...register(
+                            `utms.${index}.utm_campaign_name` as const,
+                            {
+                              maxLength: 20,
+                              required: true,
+                            }
+                          )}
+                          className={`${
+                            errors?.utms?.[index]?.utm_campaign_name
+                              ? styles.error
+                              : styles.input_style
+                          }`}
+                        />
+                      </Tooltip>
+                      <Tooltip
+                        title={
+                          '(선택) UTM에 아이디를 붙여 구분하고 싶을 때, 값을 입력해 주세요.'
+                        }
+                      >
+                        <input
+                          placeholder="ex) 20230312_UCB, 20230329_abc…"
+                          // onInput={requeirFn}
+                          {...register(
+                            `utms.${index}.utm_campaign_id` as const,
+                            {
+                              // pattern: /[a-z]/i,
+                              maxLength: 20,
+                            }
+                          )}
+                          className={`${
+                            errors?.utms?.[index]?.utm_campaign_id
+                              ? 'error'
+                              : ''
+                          }, ${styles.input_style}`}
+                        />
+                      </Tooltip>
+                      <Tooltip
+                        title={
+                          '(선택) 검색 광고를 통해 홍보한다면 그때 사용되는 검색어를 입력해 주세요.'
+                        }
+                      >
+                        <input
+                          // onInput={requeirFn}
+                          placeholder="ex) GA, UTM.."
+                          {...register(`utms.${index}.utm_term` as const, {
+                            maxLength: 20,
+                          })}
+                          className={`${
+                            errors?.utms?.[index]?.utm_campaign_id
+                              ? 'error'
+                              : ''
+                          }, ${styles.input_style}`}
+                        />
+                      </Tooltip>
+                      <Tooltip
+                        title={
+                          '(선택) 동일한 광고 캠페인에서 소재가 여러 개일 경우 구분할 수 있는 소재명을 입력해 주세요.'
+                        }
+                      >
+                        <input
+                          // onInput={requeirFn}
+                          placeholder="ex) 1st, 2nd…"
+                          {...register(`utms.${index}.utm_content` as const, {
+                            maxLength: 20,
+                          })}
+                          className={`${
+                            errors?.utms?.[index]?.utm_campaign_id
+                              ? 'error'
+                              : ''
+                          }, ${styles.input_style}`}
+                        />
+                      </Tooltip>
+                      <Tooltip
+                        title={
+                          '(선택) UTM에 대한 메모를 남길 수 있습니다. 자유롭게 활용하세요.'
+                        }
+                      >
+                        <textarea
+                          className={`${styles.active}`}
+                          placeholder="ex) 캠페인 코멘트, 세션 수 등의 정보"
+                          {...register(`utms.${index}.utm_memo` as const, {
+                            maxLength: 100,
+                          })}
+                          spellCheck={false}
+                        />
+                      </Tooltip>
+                      <div className={styles.minus_button}>
+                        <button
+                          className={styles.minus_button_style}
+                          type="button"
+                          title="button"
+                          onClick={() => {
+                            if (index >= 1) {
+                              remove(index);
+                            }
+                          }}
+                        >
+                          {/* <Image
+                            className={styles.minus_img}
+                            src={minus}
+                            alt="리스트 삭제"
+                            onError={() => {
+                              console.log(
+                                '리스트 빼기 이미지를 불러올 수 없습니다.'
+                              );
+                            }}
+                          /> */}
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={styles.create_button_box}>
+          <button
+            className={styles.add_list_button}
+            title="button"
+            type="button"
+            onClick={addList}
+          >
+            {/* <Image
+              className={styles.plus_button_img}
+              src={plus}
+              alt="추가하기"
+              onError={() => {
+                console.log('추가버튼 이미지를 불러오지 못했습니다.');
+              }}
+            /> */}
+          </button>
+          <div className={styles.create_button_box_section}>
+            {/* {alert && (
+              <Alert
+                title={'성공'}
+                contents={'UTM 생성을 성공하셨습니다!'}
+                onClickEvent={setAlert}
+              />
+            )} */}
+            {/* {isLoading && <Loading isOpen={true} />} */}
+
+            {/* <BlueButton
+              size={'sm'}
+              color={'full'}
+              text={'생성하기'}
+              types={'submit'}
+              disabled={isLoading}
+            /> */}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
