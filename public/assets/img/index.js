505d180cc1a0b22535fc139424f00eff5ccda862
Goldenprevue <goldenprevue@gmail.com>
2023-06-04 03:59:53 +0900

    ADD.signup

---
+++ public/assets/img/index.js
@@ -0,0 +1,5 @@
+import m_section_one_two from "./m_section_one_two.png";
+import sub_child from "./sub_child.png";
+import copyright from "./copyright.png";
+
+export { m_section_one_two, sub_child, copyright };
